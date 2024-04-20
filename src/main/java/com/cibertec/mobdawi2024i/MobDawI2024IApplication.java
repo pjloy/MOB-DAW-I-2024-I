bf62d15d153bb2a4fba85d3c39e4c457747e1b5d
Jhon Loyola <pjloyola@cibertec.edu.pe>
2024-04-20 09:12:59 -0500

    Black Project Spring Boot

---
+++ src/main/java/com/cibertec/mobdawi2024i/MobDawI2024IApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.mobdawi2024i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobDawI2024IApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobDawI2024IApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/mobdawi2024i/MobDawI2024IApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.mobdawi2024i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobDawI2024IApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
