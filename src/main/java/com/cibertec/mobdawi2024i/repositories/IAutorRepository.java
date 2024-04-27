f80560507461936381fabdc22f500e23687419d4
Jhon Loyola <pjloyola@cibertec.edu.pe>
2024-04-27 07:26:06 -0500

    end sesion 04

--- src/main/java/com/cibertec/mobdawi2024i/repositories/IAutorRepository.java
+++ src/main/java/com/cibertec/mobdawi2024i/repositories/IAutorRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import com.cibertec.mobdawi2024i.entities.AutorEntity;
 
 @Repository
-public interface AutorRepository extends IBaseRepository<AutorEntity, Long> {
+public interface IAutorRepository extends IBaseRepository<AutorEntity, Long> {
 
 }
--- src/main/java/com/cibertec/mobdawi2024i/repositories/IPersonaRepository.java
+++ src/main/java/com/cibertec/mobdawi2024i/repositories/IPersonaRepository.java
@@ -1,5 +1,7 @@
 package com.cibertec.mobdawi2024i.repositories;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.cibertec.mobdawi2024i.entities.PersonaEntity;
@@ -7,6 +9,14 @@ import com.cibertec.mobdawi2024i.entities.PersonaEntity;
 @Repository
 public interface IPersonaRepository extends IBaseRepository<PersonaEntity, Long> {
 	
+	// Query Method
+	// RQ: Busqueda de la persona por nombre o apellido (fragmento)
+	List<PersonaEntity> findByNameContainingOrLastnameContaining (String name, String lastname);
 	
-
+	// Query JPQL
+	
+	
+	
+	// Query con SQL Nativo
+	
 }
